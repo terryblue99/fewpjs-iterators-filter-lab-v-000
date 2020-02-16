28d76328c3cb8e068387daa70a63f5e82e42eae4
Terry Blue <terryblue@comcast.net>
2020-02-16 22:14:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const findMatching = (drivers, driverName) => {
   return drivers.filter(name => name.toLowerCase() === driverName.toLowerCase())
 }
+
+const fuzzyMatch = (drivers, firstLetters) => {
+  return drivers.filter(name => name.toLowerCase().includes(firstLetters.toLowerCase()))
+}
